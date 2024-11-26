3c0116bb0503482f8f2b1a56631c2fb5b8733c58
Stefan Sabev <stevenbg91@gmail.com>
2024-11-26 12:25:32 +0100

    Create navigation.ts
    Added abstract nav links

---
+++ src/utils/navigation.ts
@@ -0,0 +1,9 @@
+const navigation = [
+    {route : '/', name: 'About', image: "fa-regular fa-user"},
+    {route : '/resume', name: 'Resume', image: "fa-regular fa-file-lines"},
+    {route : '/portfolio', name: 'Works', image: "fas fa-briefcase"},
+    {route : '/blog', name: 'Blogs', image: "fa-brands fa-blogger"},
+    {route : '/contact', name: 'Contact', image: "fa-solid fa-address-book"},
+]
+
+export default navigation
