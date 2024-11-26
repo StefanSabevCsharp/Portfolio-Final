02f9619cbbe39cd541dcbc31840cfbc182ee2c4d
Stefan Sabev <stevenbg91@gmail.com>
2024-11-26 12:26:17 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router"
-import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import TopElement from "./components/topElement/TopElement"
+import Main from "./components/main/Main"
+import Contact from "./components/contact/Contact"
 
 
 function App() {
@@ -10,10 +12,13 @@ function App() {
     <>
       <div className="bg-homeBg dark:bg-homeTwoBg-dark min-h-screen bg-no-repeat bg-center bg-cover bg-fixed md:pb-16 w-full">
 
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <TopElement />
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Main>
       </div>
     </>
   )
