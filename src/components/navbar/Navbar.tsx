f17184777f334eb77c4bd0c037359e1b97b17037
Stefan Sabev <stevenbg91@gmail.com>
2024-11-28 12:56:15 +0100

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,49 +1,25 @@
-export default function Navbar() {
-  return (
-    <nav id="navbar" className="hidden lg:hidden">
-  <ul className="block rounded-b-[20px] shadow-md absolute left-0 top-20 z-[22222222222222] w-full bg-white dark:bg-[#1d1d1d]">
-    <li>
-      <a className="mobile-menu-items" href="aboutOne.html">
-        <span className="mr-2 text-xl">
-          <i className="fa-regular fa-user" />
-        </span>{" "}
-        About{" "}
-      </a>
-    </li>
-    <li>
-      <a className="mobile-menu-items-active" href="resumeOne.html">
-        <span className="mr-2 text-xl">
-          <i className="fa-regular fa-file-lines" />
-        </span>{" "}
-        Resume{" "}
-      </a>
-    </li>
-    <li>
-      <a className="mobile-menu-items" href="portfiloOne.html">
-        <span className="mr-2 text-xl">
-          <i className="fas fa-briefcase" />
-        </span>{" "}
-        Works{" "}
-      </a>
-    </li>
-    <li>
-      <a className="mobile-menu-items" href="blogOne.html">
-        <span className="mr-2 text-xl">
-          <i className="fa-brands fa-blogger" />
-        </span>{" "}
-        Blogs{" "}
-      </a>
-    </li>
-    <li>
-      <a className="mobile-menu-items" href="contactOne.html">
-        <span className="mr-2 text-xl">
-          <i className="fa-solid fa-address-book" />
-        </span>{" "}
-        Contact{" "}
-      </a>
-    </li>
-  </ul>
-</nav>
+import { Link, useLocation } from "react-router";
+import navigation from "../../utils/navigation";
 
-  );
+export default function Navbar() {
+    const locationPath = useLocation().pathname;
+    const activeLink: string = "mobile-menu-items-active";
+    const inactiveLink: string = "mobile-menu-items";
+    return (
+        <nav id="navbar" className="hidden lg:hidden">
+            <ul className="block rounded-b-[20px] shadow-md absolute left-0 top-20 z-[22222222222222] w-full bg-white dark:bg-[#1d1d1d]">
+                {navigation.map((link,index) => (
+                    <li key={index}>
+                    <Link className={locationPath == link.route ? activeLink : inactiveLink} to={link.route}>
+                        <span className="mr-2 text-xl">
+                            <i className={link.image} />
+                        </span>{" "}
+                        {link.name}{" "}
+                    </Link>
+                </li>
+                ))}
+            </ul>
+        </nav>
+
+    );
 }
