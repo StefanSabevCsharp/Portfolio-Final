04a6df6d356c93624fd51fb6bafbd5009dfe8bb0
Stefan Sabev <stevenbg91@gmail.com>
2024-11-28 12:56:07 +0100

    Update navigation.ts

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -2,8 +2,8 @@ const navigation = [
     {route : '/', name: 'About', image: "fa-regular fa-user"},
     {route : '/resume', name: 'Resume', image: "fa-regular fa-file-lines"},
     {route : '/portfolio', name: 'Works', image: "fas fa-briefcase"},
-    {route : '/blog', name: 'Blogs', image: "fa-brands fa-blogger"},
     {route : '/contact', name: 'Contact', image: "fa-solid fa-address-book"},
+    {route : '/awards', name: 'Awards', image: "fa-brands fa-blogger"},
 ]
 
 export default navigation
