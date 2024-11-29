9c687209fb0a9e50482bee93352cc212fc92d42b
Stefan Sabev <stevenbg91@gmail.com>
2024-11-29 12:28:12 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "../public/css/custom.css"
 import "../public/css/tailwind.css"
 import "../public/css/vendor/slick.css"
 import "../public/css/vendor/jquery.modal.min.css"
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter } from 'react-router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
