2dd4ef4d981214dee8adca8789ff18d4e91422a6
Stefan Sabev <stevenbg91@gmail.com>
2024-12-02 12:23:21 +0100

    Created util for skill section to be more abstract

---
+++ src/utils/skills.ts
@@ -0,0 +1,12 @@
+   type Skills = {
+         name: string,
+         level: number,
+         style: string
+   }
+   
+   export const skills : Skills[] = [
+        {name: "Javascript" , level: 90, style: "bg-[#5185d4] h-1 rounded-full"},
+        {name: "ReactJS" , level: 85, style: "bg-[#ca56f2] h-1 rounded-full"},
+        {name: "Web Design" , level: 75, style: "bg-[#FF6464] h-1 rounded-full"},
+        {name: "BackEnd" , level: 85, style: "bg-[#9272d4] h-1 rounded-full"},
+    ]
