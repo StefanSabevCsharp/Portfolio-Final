4fe52f465bf6b253f0c0ee39d838254860c54330
Stefan Sabev <stevenbg91@gmail.com>
2024-12-05 12:47:08 +0100

    Added resources

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,9 @@
 
+
 import Header from "../header/Header";
 import Sidebar from "../sidebar/Sidebar";
 
 export default function Main({ children }: { children: React.ReactNode }) {
-
     return (
         <div className="container grid grid-cols-12 md:gap-10 justify-between lg:mt-[220px]">
             {/* sidber personal info */}
