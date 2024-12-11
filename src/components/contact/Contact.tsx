876d195eed11fbf76175f5ec4a4fddad89cad27c
Stefan Sabev <stevenbg91@gmail.com>
2024-12-11 12:41:18 +0100

    Update Contact.tsx
    Using translations

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,6 +3,7 @@
     import emailjs from "emailjs-com";
     import { useState } from "react";
     import { isSubmittedInSession, updateSessionStorage } from "../../utils/sessionStorage";
+import { useTranslation } from "react-i18next";
 
     type ContactProps = {
         name: string;
@@ -11,6 +12,7 @@
     }
 
     export default function Contact() {
+        const { t } = useTranslation();
         const [isSubmitted, setIsSubmitted] = useState(() => isSubmittedInSession);
         const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -58,11 +60,11 @@
                     <div className="mx-4 md:mx-[60px] p-4 md:p-16 dark:border-[#212425] dark:border-2 bg-color-810 rounded-xl dark:bg-[#111111] mb-[30px] md:mb-[60px]">
                         <h3 className="text-4xl">
                             <span className="text-gray-lite dark:text-[#A6A6A6]">
-                                I'm always open for contact
+                                {t("contact1")}
                             </span>
                             <br />
                             <span className="font-semibold dark:text-white">
-                                regarding our future project together.
+                            {t("contact2")}
                             </span>
                         </h3>
                         <form onSubmit={handleSubmit} method="POST">
@@ -70,9 +72,9 @@
                                 className="returnmessage"
                                 data-success="Your message has been received, We will contact you soon."
                             />
-                            <div className="empty_notice">
+                            {/* <div className="empty_notice">
                                 <span>Please Fill Required Fields</span>
-                            </div>
+                            </div> */}
                             {/* name input  */}
                             <div className="relative z-0 w-full mt-[40px] mb-8 group">
                                 <input
@@ -89,7 +91,7 @@
                                     htmlFor="name"
                                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-color-910 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FF6464] peer-focus:dark:text-[#FF6464] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8"
                                 >
-                                    Name *{" "}
+                                   {t('nameContact')} *{" "}
                                 </label>
                             </div>
                             {/* email input  */}
@@ -108,7 +110,7 @@
                                     htmlFor="email"
                                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-color-910 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#5185D4] peer-focus:dark:text-[#FF6464] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8"
                                 >
-                                    Email *{" "}
+                                    {t('emailContact')} *{" "}
                                 </label>
                             </div>
                             {/* message input  */}
@@ -127,7 +129,7 @@
                                     htmlFor="message"
                                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-color-910 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#CA56F2] peer-focus:dark:text-[#FF6464] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8"
                                 >
-                                    Message *{" "}
+                                    {t('messageContact')} *{" "}
                                 </label>
                             </div>
                             {/* submit buttons */}
@@ -135,7 +137,7 @@
                                 type="submit"
                                 disabled={isSubmitting || isSubmitted}
                                 className="px-6 py-2 rounded-lg border-[2px] mt-3 border-color-910 font-semibold cursor-pointer hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] hover:text-white transition-colors duration-300 ease-in-out hover:border-transparent dark:text-white"
-                            value={isSubmitting ? "Sending..." :  isSubmitted ? "Submitted" : "Submit"}
+                            value={isSubmitting ? `${t('sending')}` :  isSubmitted ? `${t('submitted')}` : `${t("submit")}`}
                             />
                             
                         </form>
