268d7e0e119b47b1295fc44c3ee85722c54474a2
Stefan Sabev <stevenbg91@gmail.com>
2024-12-11 12:40:13 +0100

    Update navigation.ts
    refactored navigation so i can use translations as well

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,15 +1,22 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 type Navigation = {
     route: string,
     name: string,
     image: string
 };
 
-const navigation : Navigation[] = [
-    {route : '/', name: 'About', image: "fa-regular fa-user"},
-    {route : '/resume', name: 'Resume', image: "fa-regular fa-file-lines"},
-    {route : '/portfolio', name: 'Portfolio', image: "fas fa-briefcase"},
-    {route : '/contact', name: 'Contact', image: "fa-solid fa-address-book"},
-    {route : '/awards', name: 'Awards', image: "fa-brands fa-blogger"},
-]
+export default function useNav() : { navigation: Navigation[] } {
+    const { t } = useTranslation();
 
-export default navigation;
+   const navigation = [
+        { route: '/', name: t('navigation.about'), image: "fa-regular fa-user" },
+        { route: '/resume', name: t('navigation.resume'), image: "fa-regular fa-file-lines" },
+        { route: '/portfolio', name: t('navigation.portfolio'), image: "fas fa-briefcase" },
+        { route: '/contact', name: t('navigation.contact'), image: "fa-solid fa-address-book" },
+        { route: '/awards', name: t('navigation.awards'), image: "fa-brands fa-blogger" },
+    ];
+    return { navigation };
+
+};
