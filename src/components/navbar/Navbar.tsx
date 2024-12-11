61e31422fac5330eacf6bc0f94931b7f030a6254
Stefan Sabev <stevenbg91@gmail.com>
2024-12-11 12:41:06 +0100

    Update Navbar.tsx
    Refactored navigation so it uses translation as well

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,29 @@
 import { Link, useLocation } from "react-router";
-import navigation from "../../utils/navigation";
+import useNav from "../../utils/navigation";
+
 
 export default function Navbar() {
+
+    const {navigation} = useNav();
     const locationPath = useLocation().pathname;
     const activeLink: string = "mobile-menu-items-active";
     const inactiveLink: string = "mobile-menu-items";
+
     return (
         <nav id="navbar" className="hidden lg:hidden">
             <ul className="block rounded-b-[20px] shadow-md absolute left-0 top-20 z-[22222222222222] w-full bg-white dark:bg-[#1d1d1d]">
-                {navigation.map((link,index) => (
+                {navigation.map((link, index) =>
+                (
                     <li key={index}>
-                    <Link className={locationPath == link.route ? activeLink : inactiveLink} to={link.route}>
-                        <span className="mr-2 text-xl">
-                            <i className={link.image} />
-                        </span>{" "}
-                        {link.name}{" "}
-                    </Link>
-                </li>
-                ))}
+                        <Link className={locationPath == link.route ? activeLink : inactiveLink} to={link.route}>
+                            <span className="mr-2 text-xl">
+                                <i className={link.image} />
+                            </span>{" "}
+                            {link.name}{" "}
+                        </Link>
+                    </li>
+                )
+                )}
             </ul>
         </nav>
 
